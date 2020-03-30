d4265727eaeac1a3670ae0601d2d76ac16f167b3
Leo Gou <leo_g92@hotmail.fr>
2020-03-30 12:48:48 +0200

    answer works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
-    @answer = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to worl"]
-
-    if params[:answer] == 'I am going to work'
-      @answer[0]
-    elsif params[:answer] == "\?"
-      @answer[1]
-    else
-      @answer[2]
-    end
   end
 
   def answer
+    @answer = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work"]
+
+    if params[:ask] == 'I am going to work'
+      @answer = @answer[0]
+    elsif params[:ask] == "\?"
+      @answer = @answer[1]
+    else
+      @answer = @answer[2]
+    end
   end
 end
