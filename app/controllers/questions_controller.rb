e2ebc1b25a9a66361dc52e7fdf9c8a4ba2533bc9
Leo Gou <leo_g92@hotmail.fr>
2020-03-30 12:31:04 +0200

    nothing returns yet

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
+    @answer = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to worl"]
+
+    if params[:answer] == 'I am going to work'
+      @answer[0]
+    elsif params[:answer] == "\?"
+      @answer[1]
+    else
+      @answer[2]
+    end
+  end
+
+  def answer
   end
 end
